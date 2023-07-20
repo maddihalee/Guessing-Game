9cfddf8d4b5af2050115fb6cee1337182061fd9e
Maddi Cook <maddihcook@gmail.com>
2023-07-20 18:37:26 -0500

    setup

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,48 @@
+﻿string greeting = "Welcome to NumGuesser";
+
+Console.WriteLine(greeting);
+
+int secretNumber = 42;
+
+Console.WriteLine("Guess a number: ");
+int response = int.Parse(Console.ReadLine());
+
+if (response == secretNumber)
+{
+    Console.WriteLine("You guessed the correct number! Congratulations!");
+}
+else
+{
+    Console.WriteLine("Incorrect response. Try again!");
+    int response2 = int.Parse(Console.ReadLine());
+    if (response2 == secretNumber)
+    {
+        Console.WriteLine("You guessed the correct number! Congratulations!");
+    }
+    else
+    {
+        Console.WriteLine("Incorrect response. Try again!");
+        int response3 = int.Parse(Console.ReadLine());
+        if (response3 == secretNumber)
+        {
+            Console.WriteLine("You guessed the correct number! Congratulations!");
+        }
+        else
+        {
+            Console.WriteLine("Incorrect response. Try again!");
+            int response4 = int.Parse(Console.ReadLine());
+            if (response4 == secretNumber)
+            {
+                Console.WriteLine("You guessed the correct number! Congratulations!");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect response. Try again!");
+            }
+        }
+    }
+    ;
+}
+
+
+
